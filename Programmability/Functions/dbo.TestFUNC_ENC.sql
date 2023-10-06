4824870a5aa034d528000c58a51bdbabd51401e6
Ekaterinakor <k.kormilets@gmail.com>
2023-10-06 11:51:43 +0300

    First Commit

---
+++ Programmability/Functions/dbo.TestFUNC_ENC.sql
@@ -0,0 +1,14 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE FUNCTION [dbo].[TestFUNC_ENC]
+(
+  @Department INT
+)
+RETURNS TABLE
+AS RETURN
+(SELECT *
+  FROM HumanResources.Department d
+  WHERE
+    DepartmentID = @Department
+);
+GO
---
+++ Tables/dbo.MySampleTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MySampleTable] (
+  [ID] [int] NOT NULL,
+  [Name] [nvarchar](255) NULL,
+  [Age] [int] NULL,
+  PRIMARY KEY CLUSTERED ([ID])
+)
+ON [PRIMARY]
+GO

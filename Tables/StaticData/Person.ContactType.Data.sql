427dc41ea57dbe2c7946c2fc2e10957ea4a024d7
Jordan-Sanders <83335543+Jordan-Sanders@users.noreply.github.com>
2023-12-12 10:57:07 +0200

    Update Person.ContactType.Data.sql

--- Tables/StaticData/Person.ContactType.Data.sql
+++ Tables/StaticData/Person.ContactType.Data.sql
@@ -1,13 +1,13 @@
 ﻿SET IDENTITY_INSERT Person.ContactType ON
 GO
 INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (1, N'Accounting Manager', '2008-04-30 00:00:00.000');
-INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (2, N'Assistant Sales Agent', '2008-04-30 00:00:00.000');
+INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (2, N'Sales Agent', '2008-04-30 00:00:00.000');
 INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (3, N'Assistant Sales Representative', '2008-04-30 00:00:00.000');
 INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (4, N'Coordinator Foreign Markets', '2008-04-30 00:00:00.000');
-INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (5, N'Export Administrator', '2008-04-30 00:00:00.000');
-INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (6, N'International Marketing Manager', '2008-04-30 00:00:00.000');
-INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (7, N'Marketing Assistant', '2008-04-30 00:00:00.000');
-INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (8, N'Marketing Manager', '2008-04-30 00:00:00.000');
+INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (5, N'Administrator', '2008-04-30 00:00:00.000');
+INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (6, N'International Manager', '2008-04-30 00:00:00.000');
+INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (7, N'Assistant', '2008-04-30 00:00:00.000');
+INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (8, N'Manager', '2008-04-30 00:00:00.000');
 INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (9, N'Marketing Representative', '2008-04-30 00:00:00.000');
 INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (10, N'Order Administrator', '2008-04-30 00:00:00.000');
 INSERT Person.ContactType(ContactTypeID, Name, ModifiedDate) VALUES (11, N'Owner', '2008-04-30 00:00:00.000');
